cfc5e89dcf91249c5acc66a220e123fd2d9a2216
ahram2 <helloahram1@gmail.com>
2024-08-28 14:12:08 +0900

    bakjoon

---
+++ bakjoon/basic/10926_특수문자1.c
@@ -0,0 +1,11 @@
+// https://www.acmicpc.net/problem/10926
+#include <stdio.h>
+
+int main()
+{
+    char id[51];
+    scanf("%s", id);
+    printf("%s%c%c%c\n", id, '?', '?', '!');
+
+    return 0;
+}
---
+++ bakjoon/basic/10926_특수문자1.py
@@ -0,0 +1,4 @@
+# https://www.acmicpc.net/problem/10926
+name = input()
+
+print(name + "??!")
