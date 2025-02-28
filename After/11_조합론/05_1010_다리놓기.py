3b6683029d9d40fb9bdc7cbe1809800309fc5f0e
ahram2 <helloahram1@gmail.com>
2025-02-28 17:25:37 +0900

    250228FRI

--- After/11_조합론/05_1010_다리놓기.py
+++ After/11_조합론/05_1010_다리놓기.py
@@ -1,1 +1,19 @@
 # https://www.acmicpc.net/problem/1010 
+
+# 서쪽에 N 개 동쪽에 M 개 장소를 다리로 연결할 때 
+# 겹쳐지지 않고 최대한 많이 지을 수 있는 경우의 수 
+
+# Combination
+
+import sys
+import math 
+
+t = int(sys.stdin.readline().strip())
+result = []
+
+for _ in range(t):
+    n, m = map(int, sys.stdin.readline().split())
+    # M 개의 사이트 중에서 N 개를 선택
+    result.append(math.comb(m, n))
+
+sys.stdout.write("\n".join(map(str, result)) + "\n")
