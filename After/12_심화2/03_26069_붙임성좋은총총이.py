7954476dd0452f596828a680f149a3e275dc92b0
김아람 <ahram@Ahram-MacBook-Pro.local>
2025-05-09 13:31:24 +0900

    250509FRI

--- After/12_심화2/03_26069_붙임성좋은총총이.py
+++ After/12_심화2/03_26069_붙임성좋은총총이.py
@@ -1,2 +1,18 @@
 # https://www.acmicpc.net/problem/26069
 
+# N번을 걸쳐 사람을 만난다 
+# ChongChong 을 만난 사람은 무지개 댄스를 추게 된다 
+# 마지막에 무지개 댄스를 추는 사람을 구하는 문제 
+
+n = int(input()) # 사람들이 만난 기록의 수 
+dance_set = {'ChongChong'} # 춤추는 사람 집합 
+
+for _ in range(n):
+    a, b = input().split() # 만난 사람 이름 받기 
+    # a 또는 b 가 dance_set 에 존재하면 둘 다 추가 
+    if a in dance_set or b in dance_set:
+        dance_set.add(a)
+        dance_set.add(b)
+
+# 집합의 길이 출력 
+print(len(dance_set)) 
